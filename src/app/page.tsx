c7261b040beb31dc66b4e09c718968b48dcaeb18
shafi <shafi.bahrami.2015@gmail.com>
2023-04-25 21:28:09 +0200

    [redis], db, connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react"
 import Button from '../components/ui/Button';
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  await db.set('hello','hello')
   return (
     <div className='text-red-500'>
       <Button>
